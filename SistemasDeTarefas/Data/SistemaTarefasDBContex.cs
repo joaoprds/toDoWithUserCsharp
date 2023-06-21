7a5fe054db55c422e06381e53f1c1e51c8542913
joaoprds <joaopaulorochadesousa@gmail.com>
2023-06-21 18:22:35 -0300

    first commit

---
+++ SistemasDeTarefas/Data/SistemaTarefasDBContex.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemasDeTarefas.Models;
+
+namespace SistemasDeTarefas.Data
+{
+    public class SistemaTarefasDBContex : DbContext
+    {
+        public SistemaTarefasDBContex(DbContextOptions<SistemaTarefasDBContex> options)
+            : base(options)
+        {
+        }
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+        public DbSet<TarefaModel> Tarefas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
