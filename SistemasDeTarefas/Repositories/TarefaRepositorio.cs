fca22489c3bda8d4cf6cec5f8aeb23e39308cb31
joaoprds <joaopaulorochadesousa@gmail.com>
2023-06-30 17:30:39 -0300

    Created Crud for Tarefas and Finish Project

---
+++ SistemasDeTarefas/Repositories/TarefaRepositorio.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemasDeTarefas.Data;
+using SistemasDeTarefas.Models;
+using SistemasDeTarefas.Repositories.Interfaces;
+
+namespace SistemasDeTarefas.Repositories
+{
+    public class TarefaRepositorio : ITarefaRepositorio
+    {
+        private readonly SistemaTarefasDBContex _dbContext;
+        public TarefaRepositorio(SistemaTarefasDBContex sistemaTarefasDBContex)
+        {
+            _dbContext = sistemaTarefasDBContex;
+        }
+        public async Task<TarefaModel> BuscarPorId(int id)
+        {
+            return await _dbContext.Tarefas
+                .Include(x => x.Usuario).
+                FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public  async Task<List<TarefaModel>> BuscarTodasTarefas()
+        {
+            return await _dbContext.Tarefas
+                .Include(x => x.Usuario)
+                .ToListAsync();
+        }
+        public async Task<TarefaModel> Adicionar(TarefaModel tarefa)
+        {
+           await _dbContext.Tarefas.AddAsync(tarefa);
+            await _dbContext.SaveChangesAsync();
+
+            return tarefa;
+        }
+
+        public async Task<TarefaModel> Atualizar(TarefaModel tarefa, int id)
+        {
+            TarefaModel tarefaPorId = await BuscarPorId(id) ?? throw new Exception($"tarefa for ID:{id} not found in DataBase");
+            tarefaPorId.Nome = tarefa.Nome;
+            tarefaPorId.Descricao = tarefa.Descricao;
+            tarefaPorId.Status = tarefa.Status;
+            tarefaPorId.UsuarioId = tarefa.UsuarioId;
+
+            _dbContext.Tarefas.Update(tarefaPorId);
+            _dbContext.SaveChanges();
+
+            return tarefaPorId;
+
+        }
+
+        public async Task<bool> Apagar(int id)
+        {
+            TarefaModel tarefaPorId = await BuscarPorId(id) ?? throw new Exception($"tarefa for ID:{id} not found in DataBase");
+            _dbContext.Tarefas.Remove(tarefaPorId);
+            _dbContext.SaveChanges();
+            return true;
+        }
+    }
+}
